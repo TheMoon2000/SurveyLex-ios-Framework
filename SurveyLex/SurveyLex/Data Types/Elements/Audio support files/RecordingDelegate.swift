3a04d6c59615c61a236728879640c708daf0e65d
TheMoon2000 <jiarui.shan@berkeley.edu>
2019-05-17 13:24:26 -0700

    Added documentation

--- SurveyLex/SurveyLex/Data Types/Elements/Audio support files/RecordingDelegate.swift
+++ SurveyLex/SurveyLex/Data Types/Elements/Audio support files/RecordingDelegate.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 protocol RecordingDelegate {
+    
+    /// Called when recording has begun.
     func didBeginRecording(_ sender: RecordButton)
+    
+    /// Called when the recording button has finished recording.
     func didFinishRecording(_ sender: RecordButton, duration: Double)
+    
+    /// Error handling. This method could be called before recording began (i.e. lack of mic access) or during recording (i.e. interruption such as pressing the home button while recording).
     func didFailToRecord(_ sender: RecordButton, error: Recorder.Error)
 }
