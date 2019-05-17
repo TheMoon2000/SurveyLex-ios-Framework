8822cf89a03973d612577ee5143d6e9d9d270047
TheMoon2000 <jiarui.shan@berkeley.edu>
2019-05-17 02:05:02 -0700

    Project file updates

--- SurveyLex/SurveyLexTests/SurveyDataTests.swift
+++ SurveyLex/SurveyLexTests/SurveyDataTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import SwiftyJSON
 
 class SurveyDataTests: XCTestCase {
 
@@ -18,9 +19,9 @@ class SurveyDataTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testRatingDisplay() {
+        
+        
     }
 
     func testPerformanceExample() {
