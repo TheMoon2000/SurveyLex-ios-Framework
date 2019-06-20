47046adf11108d4103afddcf5922f978cdf89716
TheMoon2000 <jiarui.shan@berkeley.edu>
2019-06-20 10:45:49 -0400

    Updated TextFormatter

--- SurveyLex/SurveyLex/Data Types/Elements/TextFormatter.swift
+++ SurveyLex/SurveyLex/Data Types/Elements/TextFormatter.swift
@@ -15,23 +15,22 @@ class TextFormatter {
     }
     
     static func formatted(_ string: String, type: TextType) -> NSAttributedString {
-        var newString = string
+        
+        var newString = string // This mutable string will be used instead
+        
         // Convert all the special expressions into hyperlinks
-        do {
-            let linkDetector = try NSRegularExpression(pattern: "\\[!\\[.*\\]\\(.*\\)\\]\\(.+\\)", options: .dotMatchesLineSeparators)
-            var match = linkDetector.firstMatch(in: newString,
-                                               options: .init(),
-                                               range: NSMakeRange(0, newString.count))
-            while match != nil {
-                let matchString = String(newString[Range(match!.range, in: newString)!])
-                let components = matchString.components(separatedBy: ["(", ")"])
-                let link = components[components.count - 2]
-                newString = newString.replacingOccurrences(of: matchString, with: link)
-                match = linkDetector.firstMatch(in: newString, options: .init(), range: NSMakeRange(0, newString.count))
-            }
-        } catch {
-            print("invalid expression")
+        let linkDetector = try! NSRegularExpression(pattern: "\\[!\\[.*\\]\\(.*\\)\\]\\(.+\\)", options: .dotMatchesLineSeparators)
+        let matches = linkDetector.matches(in: newString,
+                                           options: .init(),
+                                           range: NSMakeRange(0, newString.count))
+        for match in matches {
+            let matchString = String(newString[Range(match.range, in: newString)!])
+            let components = matchString.components(separatedBy: ["(", ")"])
+            let link = components[components.count - 2]
+            newString = newString.replacingOccurrences(of: matchString, with: link)
         }
+        
+        newString = linkFormat(regex: "\\*\\*.+\\*\\*", input: newString)
         
         let attrTxt = NSMutableAttributedString(string: newString)
 
@@ -44,7 +43,7 @@ class TextFormatter {
         case .body:
             attributes[.font] = UIFont.systemFont(ofSize: 19, weight: .regular)
         case .consentText:
-            attributes[.font] = UIFont.systemFont(ofSize: 16.7)
+            attributes[.font] = UIFont.systemFont(ofSize: 17)
         case .title:
             attributes[.font] = UIFont.systemFont(ofSize: 22, weight: .medium)
         }
@@ -57,5 +56,24 @@ class TextFormatter {
         ], range: NSMakeRange(0, newString.count))
         
         return attrTxt
+    }
+    
+    private static func linkFormat(regex: String, input: String) -> String {
+        
+        var newString = input
+        
+        let detector = try! NSRegularExpression(pattern: regex, options: .dotMatchesLineSeparators)
+        let matches = detector.matches(in: newString,
+                                       options: .init(),
+                                       range: NSMakeRange(0, newString.count))
+        for match in matches {
+            let matchString = String(newString[Range(match.range, in: newString)!])
+            let start = matchString.index(matchString.startIndex, offsetBy: 2)
+            let end = matchString.index(matchString.endIndex, offsetBy: -2)
+            let link = matchString[start..<end]
+            newString = newString.replacingOccurrences(of: matchString, with: link)
+        }
+        
+        return newString
     }
 }
