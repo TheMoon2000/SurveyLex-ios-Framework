3e1ea1a49742221e0577c4fc72318d65a67733a5
TheMoon2000 <jiarui.shan@berkeley.edu>
2019-06-30 15:55:21 -0400

    Added info cell type

---
+++ SurveyLex/Data Types/Elements/Info/Info.swift
@@ -0,0 +1,69 @@
+//
+//  Info.swift
+//  SurveyLex
+//
+//  Created by Jia Rui Shan on 2019/6/30.
+//  Copyright © 2019 UC Berkeley. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+/// Represents the information for an info screen in a SurveyLex survey.
+class Info: Question, CustomStringConvertible {
+    
+    // Inherited
+    
+    var fragment: Fragment?
+    var completed = true
+    var parentView: SurveyViewController?
+    var isRequired = false
+    var order: (fragment: Int, question: Int)
+    
+    var type: ResponseType {
+        return .info
+    }
+    
+    var description: String {
+        return "Info screen <\(title)>"
+    }
+    
+    func makeContentCell() -> SurveyElementCell {
+        return InfoCell(info: self)
+    }
+    
+    var responseJSON: JSON {
+        return JSON() // Need to be replaced
+    }
+    
+    // Custom instance variables
+    
+    /// The title of the info screen.
+    let title: String
+    
+    /// A raw text representation of the body of the info screen.
+    let content: String
+    
+    /**
+     Construct a new info screen from the provided data.
+     - Parameters:
+     -  json: The JSON that contains all the information that makes up the info screen.
+     - order: A tuple that gives the index of the form in the survey (# fragment, # element), although it won't be displayed.
+     - fragment: The parent `Fragment` object which the info screen belongs to.
+     */
+    required init(json: JSON, order: (Int, Int), fragment: Fragment? = nil) {
+        let dictionary = json.dictionaryValue
+        
+        guard let title = dictionary["title"]?.string,
+            let content = dictionary["content"]?.string
+            else {
+                print(json)
+                preconditionFailure("Malformed info data")
+        }
+        
+        self.title = title
+        self.content = content
+        self.fragment = fragment
+        self.order = order
+    }
+}
---
+++ SurveyLex/Data Types/Elements/Info/InfoCell.swift
@@ -0,0 +1,132 @@
+//
+//  InfoCell.swift
+//  SurveyLex
+//
+//  Created by Jia Rui Shan on 2019/6/30.
+//  Copyright © 2019 UC Berkeley. All rights reserved.
+//
+
+import UIKit
+
+/// A subclass of `SurveyElementCell` that displays an info screen. Info screens are designed to occupy an *entire* fragment.
+class InfoCell: SurveyElementCell {
+
+    /// The `Info` object which the current cell is presenting.
+    var infoData: Info!
+    
+    private var title: UITextView!
+    private var separator: UIView!
+    private var content: UITextView!
+    private var continueButton: UIButton!
+    
+    init(info: Info) {
+        super.init()
+        
+        infoData = info
+        title = makeTitle()
+        separator = makeSeparator()
+        content = makeContentText()
+        continueButton = makeContinueButton()
+    }
+    
+    private func makeTitle() -> UITextView {
+        let titleText = UITextView()
+        titleText.isScrollEnabled = false
+        titleText.attributedText = TextFormatter.formatted(infoData.title, type: .title)
+        titleText.textAlignment = .center
+        titleText.isEditable = false
+        titleText.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(titleText)
+        
+        titleText.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor,
+                                       constant: 40).isActive = true
+        titleText.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor,
+                                        constant: 32).isActive = true
+        titleText.rightAnchor.constraint(equalTo: safeAreaLayoutGuide.rightAnchor,
+                                         constant: -32).isActive = true
+        
+        return titleText
+    }
+    
+    private func makeSeparator() -> UIView {
+        let separatorLine = UIView()
+        separatorLine.backgroundColor = UIColor(white: 0.8, alpha: 1)
+        separatorLine.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(separatorLine)
+        
+        separatorLine.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        separatorLine.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        separatorLine.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor).isActive = true
+        separatorLine.topAnchor.constraint(equalTo: title.bottomAnchor,
+                                           constant: 20).isActive = true
+        return separatorLine
+    }
+    
+    private func makeContentText() -> UITextView {
+        let content = UITextView()
+        content.attributedText = TextFormatter.formatted(infoData.content, type: .consentText)
+        content.textAlignment = .left
+        content.isEditable = false
+        content.dataDetectorTypes = .link
+        content.linkTextAttributes[.foregroundColor] = BLUE_TINT
+        content.isScrollEnabled = false
+        content.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(content)
+        
+        content.topAnchor.constraint(equalTo: separator.bottomAnchor,
+                                     constant: 20).isActive = true
+        content.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor,
+                                      constant: 30).isActive = true
+        content.rightAnchor.constraint(equalTo: safeAreaLayoutGuide.rightAnchor,
+                                       constant: -30).isActive = true
+        
+        return content
+    }
+    
+    private func makeContinueButton() -> UIButton {
+        let button = UIButton()
+        button.setTitle("Continue", for: .normal)
+        button.layer.cornerRadius = 4
+        button.backgroundColor = BLUE_TINT
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = .white
+        button.heightAnchor.constraint(equalToConstant: 49).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 220).isActive = true
+        
+        button.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchDown)
+        button.addTarget(self,
+                         action: #selector(buttonLifted(_:)),
+                         for: [.touchCancel, .touchUpInside, .touchUpOutside, .touchDragExit])
+        button.addTarget(self, action: #selector(nextPage), for: .touchUpInside)
+        
+        addSubview(button)
+        
+        button.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor).isActive = true
+        button.topAnchor.constraint(equalTo: content.bottomAnchor,
+                                    constant: 32).isActive = true
+        button.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor,
+                                       constant: -40).isActive = true
+        
+        return button
+    }
+    
+    @objc private func buttonPressed(_ sender: UIButton) {
+        sender.backgroundColor = BUTTON_PRESSED
+    }
+    
+    @objc private func buttonLifted(_ sender: UIButton) {
+        UIView.transition(with: sender,
+                          duration: 0.15,
+                          options: .transitionCrossDissolve,
+                          animations: { sender.backgroundColor = BLUE_TINT },
+                          completion: nil)
+    }
+    
+    @objc private func nextPage() {
+        let _ = infoData.parentView?.flipPageIfNeeded()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
