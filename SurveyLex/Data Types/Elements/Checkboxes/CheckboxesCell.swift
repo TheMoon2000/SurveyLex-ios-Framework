3d7d72f2dc4f37a81b4f5d2aeb0055b8a874e406
TheMoon2000 <jiarui.shan@berkeley.edu>
2019-07-16 21:33:47 -0400

    Now supports fold and expand actions

--- SurveyLex/Data Types/Elements/Checkboxes/CheckboxesCell.swift
+++ SurveyLex/Data Types/Elements/Checkboxes/CheckboxesCell.swift
@@ -13,17 +13,39 @@ class CheckboxesCell: SurveyElementCell {
     var checkboxData: CheckBoxes!
     private var title: UITextView!
     private var checkboxTable: CheckboxTable!
+    
     var modified = false
+    
     override var completed: Bool {
         return modified
     }
+    
+    /// Custom cell below for checkbox cell.
+    override var cellBelow: SurveyElementCell {
+        return bottomCell
+    }
+    
+    
+    // UI elements responsible for the expansion trigger.
+    private var expansionIndicator: UIImageView!
+    private var bottomCell: CheckboxesBottomCell!
+    private var expandButton: UIButton!
+    
+    // Quick access.
+    private var allowMenuCollapse: Bool {
+        return checkboxData.parentView!.survey.allowMenuCollapse
+    }
+    
+    /// Whether expansion events for the bottom row are suppressed.
+    private var suppressExpansion = false
 
     init(checkboxes: CheckBoxes) {
         super.init()
         
-        self.checkboxData = checkboxes
+        checkboxData = checkboxes
         title = makeTitle()
-        checkboxTable = makeCheckboxTable()
+        expansionIndicator = makeExpansionIndicator()
+        bottomCell = CheckboxesBottomCell(checkboxes: checkboxes, topCell: self)
     }
     
     private func makeTitle() -> UITextView {
@@ -43,31 +65,135 @@ class CheckboxesCell: SurveyElementCell {
         return titleText
     }
     
-    private func makeCheckboxTable() -> CheckboxTable {
-        let table = CheckboxTable(checkboxes: checkboxData, parentCell: self)
-        table.isScrollEnabled = false
+    private func makeExpansionIndicator() -> UIImageView {
+        let button = UIButton()
+        button.contentVerticalAlignment = .top
+        button.titleLabel?.font = .systemFont(ofSize: 16.5, weight: .medium)
+        button.setTitleColor(.darkGray, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(button)
         
-        /*
-        table.layer.borderColor = UIColor.orange.cgColor
-        table.layer.borderWidth = 1
-        */
+        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        button.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor,
+                                     constant: SIDE_PADDING).isActive = true
+        button.rightAnchor.constraint(equalTo: safeAreaLayoutGuide.rightAnchor,
+                                      constant: -SIDE_PADDING).isActive = true
         
-        table.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(table)
+        button.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 2).isActive = true
+        button.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor).isActive = true
         
-        table.topAnchor.constraint(equalTo: title.bottomAnchor,
-                                   constant: 20).isActive = true
-        table.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
-        table.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
-        let bottomConstraint = table.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -20)
-        bottomConstraint.priority = .defaultHigh
-        bottomConstraint.isActive = true
+        button.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchDown)
+        button.addTarget(self, action: #selector(buttonLifted(_:)), for: [
+            .touchUpInside,
+            .touchUpOutside,
+            .touchCancel,
+            .touchDragOutside,
+            .touchDragExit])
+        button.addTarget(self, action: #selector(buttonEventTriggered(_:)), for: .touchUpInside)
         
-        return table
+        expandButton = button
+        
+        
+        let expand = UIImageView(image: #imageLiteral(resourceName: "expand"))
+        expand.contentMode = .scaleAspectFit
+        expand.translatesAutoresizingMaskIntoConstraints = false
+        expand.widthAnchor.constraint(equalToConstant: 22).isActive = true
+        expand.heightAnchor.constraint(equalToConstant: 22).isActive = true
+        insertSubview(expand, belowSubview: button)
+        
+        expand.centerXAnchor.constraint(equalTo: button.centerXAnchor).isActive = true
+        expand.centerYAnchor.constraint(equalTo: button.centerYAnchor).isActive = true
+        
+        return expand
+    }
+    
+    // MARK: Button event handlers
+    
+    @objc private func buttonPressed(_ sender: UIButton) {
+        UIView.transition(with: self,
+                          duration: 0.1,
+                          options: .transitionCrossDissolve,
+                          animations: {
+                            self.expansionIndicator.image = #imageLiteral(resourceName: "expand_pressed")
+        },
+                          completion: nil)
+    }
+    
+    @objc private func buttonLifted(_ sender: UIButton) {
+        UIView.transition(with: self,
+                          duration: 0.15,
+                          options: .transitionCrossDissolve,
+                          animations: {
+                            self.expansionIndicator.image = #imageLiteral(resourceName: "expand")
+        }, completion: nil)
+        
+    }
+    
+    @objc private func buttonEventTriggered(_ sender: UIButton) {
+        
+        guard !suppressExpansion else {
+            return
+        }
+        
+        suppressExpansion = true
+        surveyPage?.focus(cell: self)
+        suppressExpansion = false
+        
+        toggleExpansion(sender)
+    }
+    
+    private func toggleExpansion(_ sender: UIButton) {
+        
+        if !allowMenuCollapse {
+            sender.isUserInteractionEnabled = false
+            UIView.transition(with: self,
+                              duration: 0.25,
+                              options: .transitionCrossDissolve,
+                              animations: {
+                                self.expansionIndicator.image = #imageLiteral(resourceName: "disabled 3x")
+                              }, completion: nil)
+        }
+        
+        bottomCell.expanded.toggle()
+        self.surveyPage?.expandOrCollapse(from: self)
+//        bottomCell.focus()
+        if self.bottomCell.expanded {
+            UIView.animate(withDuration: 0.25) {
+                //  This is stupid but you need to set the angle to a value slightly less than π for the indicator to always rotate on the right side. Rotation animations always takes the path that requires the least motion.
+                self.expansionIndicator.transform = CGAffineTransform(rotationAngle: .pi - 0.0001)
+            }
+            
+        } else if !allowMenuCollapse {
+            UIView.animate(withDuration: 0.25) {
+                self.expansionIndicator.transform = CGAffineTransform(rotationAngle: 0)
+            }
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    override func focus() {
+        super.focus()
+        
+        bottomCell.focus()
+        
+        if !modified && !bottomCell.expanded && !suppressExpansion {
+            toggleExpansion(expandButton)
+            
+            // Temporarily set `hasAutoExpanded` to true to block the button press event from being triggered.
+            suppressExpansion = true
+            DispatchQueue.main.asyncAfter(deadline: .now()) {
+                self.suppressExpansion = false
+            }
+        }
+    }
+    
+    override func unfocus() {
+        super.unfocus()
+        
+        bottomCell.unfocus()
     }
 
 }
